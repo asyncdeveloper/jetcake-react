65db7ba972993f1fe3b5f85a6a10835efe68737a
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-02-01 23:00:45 +0100

    Implement feature for users to logout

--- src/redux/auth/auth.actions.js
+++ src/redux/auth/auth.actions.js
@@ -1,4 +1,4 @@
-import { SIGNIN_FAILURE, SIGNIN_IN_PROGRESS, SIGNIN_SUCCESS } from "./auth.types";
+import { SIGN_OUT_FAILURE, SIGN_OUT_SUCCESS, SIGNIN_FAILURE, SIGNIN_IN_PROGRESS, SIGNIN_SUCCESS } from "./auth.types";
 
 export const signIn = (credentials) => {
     return (dispatch, getState , { getFirebase }) => {
@@ -19,4 +19,16 @@ export const signIn = (credentials) => {
 
 export const signInRequest = () => {
     return ({ type: SIGNIN_IN_PROGRESS });
+};
+
+export const signOut = () => {
+    return (dispatch, getState , { getFirebase }) => {
+        const firebase = getFirebase();
+
+        firebase.auth().signOut().then(() => {
+            dispatch({ type: SIGN_OUT_SUCCESS })
+        }).catch(err => {
+            dispatch({ type: SIGN_OUT_FAILURE, message: err.message });
+        });
+    }
 };
--- src/redux/auth/auth.reducer.js
+++ src/redux/auth/auth.reducer.js
@@ -1,8 +1,9 @@
-import { SIGNIN_FAILURE, SIGNIN_IN_PROGRESS, SIGNIN_SUCCESS } from "./auth.types";
+import { SIGN_OUT_FAILURE, SIGN_OUT_SUCCESS, SIGNIN_FAILURE, SIGNIN_IN_PROGRESS, SIGNIN_SUCCESS } from "./auth.types";
 
 const initState = {
     authError: null,
-    isLoading: false
+    isLoading: false,
+    errorMessage: null,
 };
 
 const authReducer = (state = initState, action) => {
@@ -10,9 +11,13 @@ const authReducer = (state = initState, action) => {
         case SIGNIN_IN_PROGRESS:
             return { ...state, authError: null, isLoading: true };
         case SIGNIN_SUCCESS:
-            return { ...state,  authError: null, isLoading: false };
+            return { ...state, authError: null, isLoading: false };
         case SIGNIN_FAILURE:
-            return { ...state,  authError: action.message, isLoading: false };
+            return { ...state, authError: action.message, isLoading: false };
+        case SIGN_OUT_SUCCESS:
+            return { ...state, errorMessage: null };
+        case SIGN_OUT_FAILURE:
+            return { ...state,  errorMessage: action.message };
         default:
             return state;
     }
