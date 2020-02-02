15dfbb924ef09af701c1a7021a957210a90f5cfd
Oluwaseyi Adeogun <sheyilaaw98@gmail.com>
2020-02-02 22:34:41 +0100

    Updates

---
+++ precache-manifest.70e69b6fbec2323fac16fe26c7bdf762.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "252ba4eeb270de1b40b3e88aea1e85eb",
+    "url": "/jetcake-react/index.html"
+  },
+  {
+    "revision": "9ec3a4396a7ca0cb4d43",
+    "url": "/jetcake-react/static/js/2.7b8681db.chunk.js"
+  },
+  {
+    "revision": "9c5172d2e5dd5ed8c80e569fe7741bde",
+    "url": "/jetcake-react/static/js/2.7b8681db.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83fec368952cbbf9305b",
+    "url": "/jetcake-react/static/js/main.76c6864b.chunk.js"
+  },
+  {
+    "revision": "d5d912410593f721aeb2",
+    "url": "/jetcake-react/static/js/runtime-main.0646478e.js"
+  },
+  {
+    "revision": "7e056773324cb6ea9c4eceb2e1fc3f9e",
+    "url": "/jetcake-react/static/media/hero-image.7e056773.jpg"
+  }
+]);
